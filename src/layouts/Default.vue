9d3eb5ac264bc3bf1e25999debbfda6d8ec1fa9a
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-03 10:25:55 -0500

    fix header bug

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="layout">
+    <header class="header">
      <!--Header-->
-    <section class="pb-6 flex justify-center bg-orange-600 justify-between">
-      <div class="text-right text-white inline-block pl-1 cursor-wait">Logo here</div>
-      <h1 class="text-center text-white text-3xl inline-block font-bold pt-2">Josh's Hip Hop Emporium</h1>
-      <div class="text-right text-white">
-        <a class="text-right inline-block pr-1 hover:text-gray-700 cursor-pointer">Contact Me</a>
-        <br>
-        <a class="text-right inline-block pr-1 hover:text-gray-700 cursor-pointer">Other</a>
-      </div>
-    </section>
+      <section class="pb-6 flex justify-center bg-orange-600 justify-between">
+        <div class="text-right text-white inline-block pl-1 cursor-wait">Logo here</div>
+        <h1 class="text-center text-white text-3xl inline-block font-bold pt-2">Josh's Hip Hop Emporium</h1>
+        <div class="text-right text-white">
+          <a class="text-right inline-block pr-1 hover:text-gray-700 cursor-pointer">Contact Me</a>
+          <br>
+          <a class="text-right inline-block pr-1 hover:text-gray-700 cursor-pointer">Other</a>
+        </div>
+      </section>
+    </header>
+    <slot/>
   </div>
 </template>
 

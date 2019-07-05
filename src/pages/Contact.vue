3591129b64630340fbeea8d7511b1cd6e99d6f62
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-05 14:23:38 -0500

    Style Contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,15 @@
 <template>
   <Layout class="">
-    <section class="text-gray-900 h-screen mx-4 my-6 px-6">
-        <div class="text-center text-5xl">Contact</div>
-        <ul class="flex flex-col text-xl list-disc">
+    <section class="text-gray-900 mx-4 my-6 px-6">
+        <div class="text-center text-5xl mb-8">Contact</div>
+        <ul class="flex flex-col text-xl list-disc bg-orange-200 p-4 border border-blue-600 mb-20">
             <li class="m-4">Email: joshbestor@utexas.edu</li>
             <li class="m-4">LinkedIn: <a href="https://www.linkedin.com/in/joshua-bestor/" class="underline text-blue-600">https://www.linkedin.com/in/joshua-bestor/</a></li>
             <li class="m-4">Phone: 830-777-3983</li>
+            <li class="m-4">Snapchat: @bestorthanu</li>
+            <li class="m-4">GitHub: <a href="https://github.com/JoshBestor/" class="underline text-blue-600">https://github.com/JoshBestor/</a></li>
         </ul>
+        <div class="text-xs text-white cursor-default">spacing</div>
     </section>
   </Layout>
 </template>

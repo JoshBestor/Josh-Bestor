bd8c6439b212cfe786ba758f9ed8422bad27128a
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-05 13:28:39 -0500

    Add the content for home and projects pages

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,11 @@
     <section class="text-gray-900 h-screen mx-4 my-6 px-6">
       <div class="text-center">Add a picture here</div>
       <div class="text-center text-5xl">About me</div>
+      <div>I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</div>
+      <div>Alara picture here</div>
+      <div>During the summer of 2019, I had an internship with Alara Creative where I learned the basics of web development.</div>
+      <div>picture of soccer here</div>
+      <div>I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</div>
     </section>
   </Layout>
 </template>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,8 +1,13 @@
 <template>
   <Layout class="">
     <section class="text-gray-900 h-screen mx-4 my-6 px-6">
-        <div class="text-center text-5xl">Projects</div>
+        <div class="text-center text-5xl">Projects</div><!--flex with boxes with title and description of project-->
         <a href="/jhhe/">Josh's Hip Hip Emporium</a>
+        <div>This is an extention of this website where I practiced web development techniques without much care for the design.</div>
+        <div>Autonomous Vehicle</div>
+        <div>This was the final project for the Intro to Electrical Engineering class. The vehicle was able to follow a black line on the ground. It was created using an arduino and various sensors.</div>
+        <div>Video Game(in C)</div>
+        <div>This was the final project for the Intro to Embedded Systems class. I recreated the two player game Nidhogg. It was created by using a Texas Intruments microcontroller, a screen, and button and joysticks for inputs.</div>
     </section>
   </Layout>
 </template>

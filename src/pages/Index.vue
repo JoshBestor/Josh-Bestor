861a818e1b280197e44b65bd2a9cbd349ee0ef56
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-05 15:16:32 -0500

    Add pictures in home and fix styling in projects

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,18 @@
 <template>
   <Layout class="">
-    <section class="text-gray-900 h-screen mx-4 my-6 px-6">
-      <div class="text-center">Add a picture here</div>
-      <div class="text-center text-5xl">About me</div>
+    <section class="text-gray-900 mx-4 my-6 px-6">
+      <div class="text-center text-5xl mb-8">About me</div>
+      <div class="bg-orange-200 p-2 flex justify-center">
+        <img src="https://i.imgur.com/GmyYFHz.jpg" alt="Picture of me" class="inline-block">
+      </div>
       <div>I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</div>
-      <div>Alara picture here</div>
+      <div class="bg-orange-200 p-2 flex justify-center">
+        <img src="https://i.imgur.com/Ombyz8w.png" alt="Alara Creative logo">
+      </div>
       <div>During the summer of 2019, I had an internship with Alara Creative where I learned the basics of web development.</div>
-      <div>picture of soccer here</div>
+      <div class="bg-orange-200 p-2 flex justify-center">
+        <img src="https://i.imgur.com/YWha0TW.jpg" alt="Chelsea FC flag">
+      </div>
       <div>I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</div>
     </section>
   </Layout>

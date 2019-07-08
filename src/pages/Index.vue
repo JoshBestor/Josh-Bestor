86c0aa6ab058faa2db31e2f51bc11b1157acbb12
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-08 09:19:28 -0500

    Style home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,18 +2,35 @@
   <Layout class="">
     <section class="text-gray-900 mx-4 my-6 px-6">
       <div class="text-center text-5xl mb-8">About me</div>
-      <div class="bg-orange-200 p-2 flex justify-center">
-        <img src="https://i.imgur.com/GmyYFHz.jpg" alt="Picture of me" class="inline-block">
+      <div class="bg-orange-200 p-8 flex flex-col border border-blue-600 mb-8">
+        <div class="bg-white border border-blue-600 p-2 mb-4 rounded">
+          <div class="flex justify-center">
+            <div class="text-2xl px-2">Education</div>
+          </div>
+          <div class="p-2 flex justify-center">
+            <img src="https://i.imgur.com/GmyYFHz.jpg" alt="Picture of me" class="inline-block border border-blue-600">
+          </div>
+          <div class="bg-white rounded">I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</div>
+        </div>
+        <div class="bg-white border border-blue-600 p-2 mb-4 rounded">
+          <div class="flex justify-center">
+            <div class="text-2xl px-2">Industry Experience</div>
+          </div>
+          <div class="p-2 flex justify-center">
+            <img src="https://i.imgur.com/Ombyz8w.png" alt="Alara Creative logo" class="inline-block border border-blue-600">
+          </div>
+          <div class="bg-white rounded">During the summer of 2019, I had an internship with Alara Creative through the <a class="text-blue-600 underline" href="https://ic2.utexas.edu/programs/home-to-texas/">Home to Texas</a> program with the University of Texas where I learned the basics of web development.</div>
+        </div>
+        <div class="bg-white border border-blue-600 p-2 rounded">
+          <div class="flex justify-center">
+            <div class="text-2xl px-2">Soccer</div>
+          </div>
+          <div class="p-2 flex justify-center">
+            <img src="https://i.imgur.com/YWha0TW.jpg" alt="Chelsea FC flag" class="inline-block border border-blue-600">
+          </div>
+          <div class="bg-white rounded">I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</div>
+        </div>
       </div>
-      <div>I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</div>
-      <div class="bg-orange-200 p-2 flex justify-center">
-        <img src="https://i.imgur.com/Ombyz8w.png" alt="Alara Creative logo">
-      </div>
-      <div>During the summer of 2019, I had an internship with Alara Creative where I learned the basics of web development.</div>
-      <div class="bg-orange-200 p-2 flex justify-center">
-        <img src="https://i.imgur.com/YWha0TW.jpg" alt="Chelsea FC flag">
-      </div>
-      <div>I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</div>
     </section>
   </Layout>
 </template>

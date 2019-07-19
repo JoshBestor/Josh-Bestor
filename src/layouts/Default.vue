cd4e6ee10af6e35522f36cb4acceb7ced80fde11
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-19 10:16:44 -0500

    Add logo

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,13 +2,13 @@
   <div>
     <header>
     <!--Header-->
-      <section class="pb-6 flex justify-center bg-orange-200 justify-between border-b border-blue-600">
+      <section class="flex justify-center bg-orange-200 justify-between border-b border-blue-600 px-2">
         <div>
-            <a href="/" class="font-serif text-blue-600 text-3xl inline-block pt-2 pl-2 hover:text-blue-800">Josh Bestor</a>
+          <a href="/" class=""><img class="h-32" src="https://i.imgur.com/EQfkhu9.png?1"></a>
         </div>
-        <div class="text-right text-gray-900 flex justify-right text-2xl pt-3">
-          <a href="/projects" class="mx-2 text-right inline-block pr-1 hover:text-gray-600 cursor-pointer">Projects</a>            
+        <div class="text-right text-gray-900 flex flex-col justify-right text-2xl pt-3">
           <a href="/contact" class="mx-2 text-right inline-block pr-1 hover:text-gray-600 cursor-pointer">Contact Me</a>
+          <a href="/projects" class="mx-2 text-right inline-block pr-1 hover:text-gray-600 cursor-pointer">Projects</a>            
         </div>
       </section>
     </header>
@@ -17,11 +17,11 @@
     </main>
     <!--Footer-->
         <div class="w-full bg-orange-200 text-gray-900 flex flex-col md:flex-row justify-center justify-between border-t border-blue-600">
-            <a href="/" class="block text-blue-600 font-serif text-center text-xl py-3 hover:text-blue-800">Josh Bestor</a>
-            <div class="block text-center pb-2 md:order-first mt-4 md:w-1/3">Designing mediocre at best websites since yesterday</div>
-            <div class="block text-center pb-6 mt-4 md:w-1/3">
-              <a class="text-blue-600 hover:text-blue-800" href="/contact">Contact</a> me through email or <a class="text-blue-600 hover:text-blue-800" href="https://www.linkedin.com/in/joshua-bestor/">LinkedIn</a>
-            </div>
+          <a href="/" class="flex justify-center"><img class="h-20" src="https://i.imgur.com/EQfkhu9.png?1"></a>
+          <div class="block text-center pb-2 md:order-first mt-4 md:w-1/3">Designing mediocre at best websites since yesterday</div>
+          <div class="block text-center pb-6 mt-4 md:w-1/3">
+            <a class="text-blue-600 hover:text-blue-800" href="/contact">Contact</a> me through email or <a class="text-blue-600 hover:text-blue-800" href="https://www.linkedin.com/in/joshua-bestor/">LinkedIn</a>
+          </div>
         </div>
         <div class="text-center text-gray-900 w-full bg-orange-200 text-xs py-4">Copyright © 2019 Joshua Bestor. All Rights Reserved.</div>
   </div>

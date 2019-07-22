3bd8bcd817d1831e288a57f22dfcee3e92929862
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-07-22 15:19:37 -0500

    Add disclaimer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,8 @@
     <section class="text-gray-900 mx-4 my-6 px-6">
       <div class="text-center text-5xl mb-8">About me</div>
       <div class="bg-orange-200 p-8 flex flex-col border border-blue-600 mb-8">
-        <aboutMe title="Education" image="https://i.imgur.com/GmyYFHz.jpg" alternate="Picture of me">I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</aboutMe>
+        <aboutMe title="Important Note" image="https://i.imgur.com/GmyYFHx.jpg" alternate="Random Guy">The information and pictures on this page are far from complete. This is just a start.</aboutMe>
+        <aboutMe title="Education" image="https://i.imgur.com/GmyYFHz.jpg" alternate="Random Tech Guy">I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</aboutMe>
         <aboutMe title="Industry Experience" image="https://i.imgur.com/Ombyz8w.png" alternate="Alara Creative logo">During the summer of 2019, I had an internship with Alara Creative through the <a class="text-blue-600 hover:text-blue-800" href="https://ic2.utexas.edu/programs/home-to-texas/" target="_blank">Home to Texas</a> program with the University of Texas where I learned the basics of web development.</aboutMe>
         <aboutMe title="Soccer" image="https://i.imgur.com/YWha0TW.jpg" alternate="Chelsea FC flag">I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</aboutMe>
       </div>

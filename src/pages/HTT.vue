57a083377ef63389151b02140cee293f948d1c1f
JoshBestor <joshbestor@utexas.edu>
2019-07-29 11:35:21 -0500

    Add HTT file

---
+++ src/pages/HTT.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout class="">
+    <section class="text-gray-900 mx-4 my-6 px-6">
+        <h1 class="text-center text-5xl mb-8">Home to Texas</h1>
+        <div class="bg-orange-200 p-6 sm:p-8 border border-blue-600 mb-20">
+          <p>content coming soon maybe</p>
+        </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home to Texas',
+    meta: [
+      {
+        key: 'description',
+        name: 'description',
+        content: 'A description of my part in the inaugural Summer of the Home to Texas program.'
+      },
+    ]
+  }
+}
+</script>

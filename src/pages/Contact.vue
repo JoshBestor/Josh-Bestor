b5043cf7ab37ad50f69c07f0e3547f056a45d9ea
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-08-18 22:08:48 -0500

    Update contact and incomplete

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -6,7 +6,6 @@
           <ul class="flex flex-col text-xl list-disc bg-white px-4 border border-blue-600 rounded-lg">
               <li class="m-4">Email: <br class="sm:hidden">joshbestor@utexas.edu</li>
               <li class="m-4">Phone: <br class="sm:hidden">830-777-3983</li>
-              <li class="m-4">Snapchat: <br class="sm:hidden">@bestorthanu</li>
               <li class="m-4">LinkedIn: <br class="sm:hidden"><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/joshua-bestor/" class="text-blue-600 hover:text-blue-300">Joshua Bestor</a></li>
               <li class="m-4">GitHub: <br class="sm:hidden"><a target="_blank" rel="noreferrer" href="https://github.com/JoshBestor/" class="text-blue-600 hover:text-blue-300">JoshBestor</a></li>
           </ul>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,8 @@
   <Layout class="">
     <section class="text-gray-900 mx-4 my-6 px-6">
       <h1 class="text-center text-5xl mb-8">About me</h1>
+      <div class="border border-red-800 rounded-lg bg-red-400 text-white text-center mb-6 p-2">This website is incomplete, there may be a few bugs</div>
       <div class="bg-orange-200 p-8 flex flex-col border border-blue-600 mb-8">
-        <aboutMe title="Important Note" image="https://i.imgur.com/GmyYFHz.jpg" alternate="Random Guy">The information and pictures on this page are far from complete. This is just a start.</aboutMe>
         <aboutMe title="Education" image="https://i.imgur.com/GmyYFHz.jpg" alternate="Random Tech Guy">I am an Electrical and Computer Engineering student at the University of Texas who is specializing in Computer Architecture and Embedded Systems along with Software Design.</aboutMe>
         <aboutMe title="Industry Experience" image="https://i.imgur.com/Ombyz8w.png" alternate="Alara Creative logo">During the summer of 2019, I had an internship with Alara Creative through the <a class="text-blue-600 hover:text-blue-300" href="https://ic2.utexas.edu/programs/home-to-texas/" target="_blank" rel="noreferrer">Home to Texas</a> program with the University of Texas where I learned the basics of web development.</aboutMe>
         <aboutMe title="Soccer" image="https://i.imgur.com/YWha0TW.jpg" alternate="Chelsea FC flag">I enjoy watching and playing soccer. I support the London based soccer team Chelsea FC. During college, I captain a coed and a mens intramural soccer team. During the summer I captain a soccer team in the Kerrville adult soccer league.</aboutMe>

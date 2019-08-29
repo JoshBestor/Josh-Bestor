25979879c936140134d71e8ae6a4323c292bfec8
JoshBestor <36572939+JoshBestor@users.noreply.github.com>
2019-08-29 12:12:15 -0500

    Add plagiarism project

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -7,6 +7,7 @@
           <project uri='/projects/vehicle' title='Autonomous Vehicle' image='https://i.imgur.com/IVY2oZB.jpg' alternate='Picture of the Vehicle' text='This was the final project for the Intro to Electrical Engineering class. The vehicle was able to follow a black line on the ground. It was created using an arduino and various sensors.'></project>
           <project uri='/projects/game' title='Video Game(in C)' image='https://i.imgur.com/AD11eGN.jpg' alternate='Nidhogg icon' text='This was the final project for the Intro to Embedded Systems class. I recreated the two player game Nidhogg. It was created by using a Texas Intruments microcontroller, a screen, and button and joysticks for inputs.'></project>          
           <project uri='/projects/site' title='This Website' image='https://i.imgur.com/Anfau8s.png' alternate='Picture of the home screen' text='This is the website you are on right now. I created it to test my web development skills. It is a Gridsome project that uses TailwindCSS and Vue.js.'></project>
+          <project uri='/projects/plagiarism' title='Plagiarism Detection Software' image='https://i.imgur.com/0AwzHbx.png' alternate='Picture' text='This was the final project for the Software Design and Implementation I class. The software takes up to 5,000 files and returns the files that share phrases.'></project>
         </div>
     </section>
   </Layout>
---
+++ src/pages/Projects/Plagiarism.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout class="">
+    <section class="text-gray-900 mx-4 my-6 px-6">
+        <h1 class="text-center text-5xl mb-8">Plagiarism Detection Software</h1>
+        <projectAbout>
+          <p class="">
+            This is the final project for the Software Design and Implementation I class that I took during the Spring of 2019. In the class, I advanced my understanding of C programming and discovered C++ and Object Oriented Programming. The program runs on Linux servers. To use it, compile the file and type ./plagiarismCatcher 'path/to/files' 'x' 'y'. The path to files is how to reach the files that you want to test for plagiarism. The variable 'x' is how many words you want per phrase of text to test and the variable 'y' is the minimum amount of collisions that you concider plagiarism.
+          </p>
+          <br>
+          <p>
+            The plagiarism catcher uses a hashtable of linked lists to store the file name where the phrases came from. Whenever there are two phrases that are the same, there will be both file names in the same location in the hashtable. Once this has been done, the software counts how many collisions each file has with every other file in the set and returns the files that have more than the specified ammount. 
+          </p>
+        </projectAbout>
+        <gitHubLink link="https://github.com/JoshBestor/Project_8_Cheaters">Content is under plagiarismCatcher.cpp</gitHubLink>
+    </section>
+  </Layout>
+</template>
+
+<script>
+import projectAbout from '~/components/Project_About.vue'
+import gitHubLink from '~/components/GitHub_Link.vue'
+
+export default {
+  metaInfo: {
+    title: 'Projects',
+    meta: [
+      {
+        key: 'description',
+        name: 'description',
+        content: 'A description of the website that you are currently on.'
+      },
+    ]
+  },
+
+  components: {
+    projectAbout,
+    gitHubLink
+  }
+}
+</script>
